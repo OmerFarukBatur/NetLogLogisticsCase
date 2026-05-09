91d1dbf4c018b5a3797d702d7a62cc324be069ed
Ömer Faruk BATUR <44163877+OmerFarukBatur@users.noreply.github.com>
2026-05-09 14:48:19 +0300

    Add project files.

---
+++ DistributionCaseStudy/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DistributionCaseStudy.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
